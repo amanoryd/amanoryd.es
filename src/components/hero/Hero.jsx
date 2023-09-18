e5defe50d9116ea171b6752558f92a2511eee79e
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-09-18 18:57:24 +0200

    uploading content

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -28,47 +28,50 @@ function Hero() {
   return (
 		<>
       <section className="home">
-        <img decoding="async" className="img-slide active" src="1.webp" ></img>
-        <img decoding="async" className="img-slide" src="2.webp" ></img>
-        <img decoding="async" className="img-slide" src="3.webp" ></img>
-        <img decoding="async" className="img-slide" src="4.webp" ></img>
-        <img decoding="async" className="img-slide" src="5.jpg" ></img>
+
+        <video className="img-slide" src="./public/1.mp4" autoPlay muted loop></video>
+        <video className="img-slide" src="./public/2.mp4" autoPlay muted loop></video>
         
         <div className="content active">
-          <h1>USSF-44.<br /><span>Mission</span></h1>
+          <h1>Profesionales de la<br /><span>Transformación</span></h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-            tempor incididunt ut labore et dolore magna anime. Ut enim ad minim 
-            veniam, quis nostrud exercitation ullamco laboris nisi ut farhan ex ea 
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-            velit esse cillum daily web design nulla pariatur. Excepteur sint 
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum.
+            Calidad, confianza y resultados excepcionales. Descubre por qué somos la elección 
+            número uno para reformas en Madrid.
           </p>
           <a href="#">Read More</a>
         </div>
 
         <div className="content">
-          <h1>STARSHIP UPDATE.<br /><span>Rocket</span></h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna anime. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut farhan ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum daily web design nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1>Hogar<br /><span>Renovado</span></h1>
+          <p>
+            Creamos espacios que inspiran y mejoran la vida de nuestros clientes.
+          </p>
           <a href="#">Read More</a>
         </div>
 
         <div className="content">
-          <h1>INTELSAT.<br /><span>G-31MISSION</span></h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna anime. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut farhan ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum daily web design nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1>Reformas<br /><span>Personalizadas</span></h1>
+          <p>
+            Somos un equipo de expertos en reformas, con años de experiencia y una pasión por el 
+            diseño y la funcionalidad. Nuestro enfoque personalizado garantiza que tu espacio 
+            refleje tu estilo y necesidades.
+          </p>
           <a href="#">Read More</a>
         </div>
 
         <div className="content">
-          <h1>NASA ASTRONAUTS.<br /><span>THE MOON</span></h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna anime. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut farhan ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum daily web design nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1>Hogar mejorado.<br /><span>Vida mejorada.</span></h1>
+          <p>
+            Déjanos hacer realidad tus ideas y crear el hogar de tus sueños.
+          </p>
           <a href="#">Read More</a>
         </div>
 
         <div className="content">
-          <h1>Space Travel.<br /><span>Rocket</span></h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna anime. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut farhan ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum daily web design nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1>Somos<br /><span>@mano, reformas y decoración</span></h1>
+          <p>
+
+          </p>
           <a href="#">Read More</a>
         </div>
 

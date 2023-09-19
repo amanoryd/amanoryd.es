a7cf97e7754a12465ee852612a070abbb9241763
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-09-19 13:21:15 +0200

    actualización de código
    act

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,97 +1,81 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './Hero.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function Hero() {
+  const [activeSlide, setActiveSlide] = useState(0);
 
-  const menuBtn = useRef(null);
-  const navigation = useRef(null);
-
-  useEffect(() => {
-    const handleMenuClick = () => {
-      menuBtn.current.classList.toggle('active');
-      navigation.current.classList.toggle('active');
-    };
-
-    // Verifica si los elementos existen antes de agregar eventos
-    if (menuBtn.current && navigation.current) {
-      menuBtn.current.addEventListener('click', handleMenuClick);
-    }
-
-    // Limpia el evento cuando el componente se desmonta
-    return () => {
-      if (menuBtn.current && navigation.current) {
-        menuBtn.current.removeEventListener('click', handleMenuClick);
-      }
-    };
-  }, []);
+  const sliderNav = (manual) => {
+    setActiveSlide(manual);
+  };
 
   return (
-		<>
+    <div className="container">
       <section className="home">
+        {[0, 1, 2, 3, 4].map((index) => (
+          <video
+            key={index}
+            className={`video-slide ${index === activeSlide ? 'active' : ''}`}
+            src={`./public/${index}.mp4`}
+            autoPlay
+            muted
+            loop
+          ></video>
+        ))}
 
-        <video className="img-slide" src="./public/1.mp4" autoPlay muted loop></video>
-        <video className="img-slide" src="./public/2.mp4" autoPlay muted loop></video>
-        
-        <div className="content active">
-          <h1>Profesionales de la<br /><span>Transformación</span></h1>
-          <p>
-            Calidad, confianza y resultados excepcionales. Descubre por qué somos la elección 
-            número uno para reformas en Madrid.
-          </p>
-          <a href="#">Read More</a>
-        </div>
-
-        <div className="content">
-          <h1>Hogar<br /><span>Renovado</span></h1>
-          <p>
-            Creamos espacios que inspiran y mejoran la vida de nuestros clientes.
-          </p>
-          <a href="#">Read More</a>
-        </div>
-
-        <div className="content">
-          <h1>Reformas<br /><span>Personalizadas</span></h1>
-          <p>
-            Somos un equipo de expertos en reformas, con años de experiencia y una pasión por el 
-            diseño y la funcionalidad. Nuestro enfoque personalizado garantiza que tu espacio 
-            refleje tu estilo y necesidades.
-          </p>
-          <a href="#">Read More</a>
-        </div>
-
-        <div className="content">
-          <h1>Hogar mejorado.<br /><span>Vida mejorada.</span></h1>
-          <p>
-            Déjanos hacer realidad tus ideas y crear el hogar de tus sueños.
-          </p>
-          <a href="#">Read More</a>
-        </div>
-
-        <div className="content">
-          <h1>Somos<br /><span>@mano, reformas y decoración</span></h1>
-          <p>
-
-          </p>
-          <a href="#">Read More</a>
-        </div>
+        {[0, 1, 2, 3, 4].map((index) => (
+          <div key={index} className={`content ${index === activeSlide ? 'active' : ''}`}>
+            <h1>
+              {index === 0 && 'Profesionales'}
+              {index === 1 && 'Hogar'}
+              {index === 2 && 'Reformas'}
+              {index === 3 && 'Hogar Mejorado.'}
+              {index === 4 && 'Somos'}
+              <br />
+              <span>{index === 0 && 'de la Transformación.'}</span>
+              <span>{index === 1 && 'Renovado'}</span>
+              <span>{index === 2 && 'Personalizadas'}</span>
+              <span>{index === 3 && 'Vida Mejorada.'}</span>
+              <span>{index === 4 && 'Amano, reformas y decoración.'}</span>
+            </h1>
+            <p>
+              {index === 0 &&
+                'Calidad, confianza y resultados excepcionales. Descubre por qué somos la elección número uno para reformas en Madrid.'}
+              {index === 1 && 'Creamos espacios que inspiran y mejoran la vida de nuestros clientes.'}
+              {index === 2 &&
+                'Somos un equipo de expertos en reformas, con años de experiencia y una pasión por el diseño y la funcionalidad. Nuestro enfoque personalizado garantiza que tu espacio refleje tu estilo y necesidades.'}
+              {index === 3 && 'Déjanos hacer realidad tus ideas y crear el hogar de tus sueños.'}
+              {index === 4 && ''}
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        ))}
 
         <div className="media-icons">
-          <a href="#"><i className="fab fa-facebook-f"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="https://www.facebook.com/amanoreformas/">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a href="https://www.instagram.com/amano_reformas/">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://www.twitter.com/amanoryd/">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
         </div>
 
         <div className="slider-navigation">
-          <div className="nav-btn active"></div>
-          <div className="nav-btn"></div>
-          <div className="nav-btn"></div>
-          <div className="nav-btn"></div>
-          <div className="nav-btn"></div>
+          {[0, 1, 2, 3, 4].map((index) => (
+            <div
+              key={index}
+              className={`nav-btn ${index === activeSlide ? 'active' : ''}`}
+              onClick={() => sliderNav(index)}
+            ></div>
+          ))}
         </div>
-
       </section>
-		</>
-  )
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;

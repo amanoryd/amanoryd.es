36d8325152782361ab714bf2494978c7ca004c78
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-09-21 17:40:47 +0200

    Agregada sección 1 de la web
    Este commit es solo para declarar que se está agregando la sección 1 del website, aún está en desarrollo.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Contenido del website
 import Nav from './components/nav/Nav';
 import Hero from './components/hero/Hero';
+import Section1 from './components/section1/Section1';
+
 import './index.css';
 import './Breakpoints.css';
 
@@ -9,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Nav />
     <Hero />
+    <Section1 />
   </React.StrictMode>,
 );

6677357f3d68fbfecd7350685a90a71623d80608
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-09-26 13:13:53 +0200

    mejoras de imagenes Section1

--- src/components/section1/Section1.jsx
+++ src/components/section1/Section1.jsx
@@ -12,7 +12,7 @@ function Section1() {
             encomendado la reforma de sus viviendas y negocios, siempre siendo
             consecuentes con nuestra filosofía de trabajo.
           </p>
-					<img src="./section1-img/COCINA-FRENTE-2.webp" alt="Cocina hecha a medida" width={'100%'}/>
+					<img src="./section1-img/mockup/242shots_so.webp" alt="Cocina hecha a medida" width={'100%'}/>
         </div>
 				
 				<div className="l1-2">
@@ -22,20 +22,20 @@ function Section1() {
 						reformas e interiorismo que trabajamos en conjunto para obtener los mejores 
 						resultados.
 					</p>
-					<img src="./section1-img/meeting-business.webp" alt="Profesionales de las reformas" width={'100%'}/>
+					<img src="./section1-img/mockup/351shots_so.webp" alt="Profesionales de las reformas" width={'100%'}/>
 				</div>
       </div>
 
       <div className="right1">
 				<div className="r1-1">
-					<img src="./section1-img/3-REFORMAS-INTEGRALES.webp" alt="Diseño 3D" width={'100%'}/>
+					<img src="./section1-img/mockup/10shots_so.webp" alt="Diseño 3D" width={'100%'}/>
 					<h1>Diseño 3D</h1>
 					<p>
 						Realizamos los proyectos para que el cliente pueda verlos terminados incluso 
 						antes de comenzar la obra. Diseñamos con nuestros clientes todos los espacios 
 						y los trasladamos a la obra para que el resultado sea el esperado por ellos.
 					</p>
-					<img src="./section1-img/A.webp" alt="Visualiza tu proyecto terminado antes de comenzarlo" width={'100%'}/>
+					<img src="./section1-img/mockup/801shots_so.webp" alt="Visualiza tu proyecto terminado antes de comenzarlo" width={'100%'}/>
 				</div>
 			</div>
     </div>

17a01bfa524f70b64bf3ebdbca1f0de1a132fc8c
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-10-09 17:57:59 +0200

    estilizando la sección de blog en la pagina principal
    Queda pendiente establecer parámetros para traer las entradas del blog con una API

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -5,7 +5,12 @@ function Blog() {
   return (
     <div className='blog1'>
       <div className='blog-container'>
-        Blog
+        <h4>BLOG</h4>
+        <h1>Últimas entradas</h1>
+        <p>Mantente al día con las últimas tendencias en Cocinas, Baños, Salones y Dormitorios.</p>
+      </div>
+      <div>
+        AQUI LAS ENTRADAS DEL BLOG
       </div>
     </div>
   )

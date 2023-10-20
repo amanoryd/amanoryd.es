439585bdc9c99eff2b047fbab61ba448d3202f1b
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-10-20 18:45:45 +0200

    Formateando el renderizado del JSON en el Blog Se ha pasado la función para renderizar y traer la información del JSON del Blog de Wordpress a esta webApp.
    Sólo queda pendiente darle formato con CSS.

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,31 +1,53 @@
 import React from "react";
 import { useFetch } from "../../useFetch";
-import './Blog.css';
+import "./Blog.css";
 
 function Blog() {
-  const { data } = useFetch('https://amanoryd.es/wp-json/wp/v2/posts');
+  // URL Origen con la info del Blog en JSON.
+  const { data } = useFetch("https://amanoryd.es/wp-json/wp/v2/posts");
+
+  // función para cambiar el formato de la fecha que viene del JSON.
+  const formatFecha = (fecha) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(fecha).toLocaleDateString('es-ES', options);
+  };
 
   return (
-    <div className='blog1'>
-      <div className='blog-container'>
+    <div className="blog1">
+      <div className="blog-container">
         <h4>BLOG</h4>
         <h1>Últimas entradas</h1>
-        <p>Mantente al día con las últimas tendencias en Cocinas, Baños, Salones y Dormitorios.</p>
+        <p>
+          Mantente al día con las últimas tendencias en Cocinas, Baños, Salones
+          y Dormitorios.
+        </p>
       </div>
 
-      <br /><br />
+      <br />
+      <br />
 
       <div>
-        <h2>AQUI LAS ENTRADAS DEL BLOG</h2><br /><br />
-        <ul>
-          {data && data.map(post => (
-            <li key={post.id}>{post.title.rendered}</li>
-          ))}
-        </ul>
+
+        <div>
+          <ul>
+            {data &&
+              data.map((post) => 
+                <div key={post.id}>
+                  <img src={post.uagb_featured_image_src.medium[0]} alt="" />
+                  <div>{post.title.rendered}</div>
+                  <div>{post.yoast_head_json.description}</div>
+                  <div>{post.yoast_head_json.author}</div>
+                  <div>{formatFecha(post.date)}</div>
+                  <br /><br />
+                </div>
+              )
+            }
+          </ul>
+        </div>
 
       </div>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;

8177072a69c7bb41b6bba17711dc90f78eb709c1
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-12-20 18:49:25 +0100

    Actualización de la sección 'Proyectos'

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import "./Proyectos.css";
-import PanoramicView from "./PanoramicView";
 
 function Proyectos() {
   return (
     <section className="proyectos-container" id="proyectos">
       <h1>Proyectos</h1>
-      <div className="proyectos-wrap-top">
-        <PanoramicView src={'http://renderstuff.com/tools/360-panorama-web-viewer-embed/?image=https://amanoryd.netlify.app/panoramic/prueba-panoramica-360.png'}/>
-        <PanoramicView src={'https://panoraven.com/en/embed/dgE8z7c9u3'}/>
+      <div className="proyectos-wrapper">
+        <div className="parent">
+          <div className="div1">
+            <img src="./proyectos/1-1/render-cocina-1-1-0001.jpg" alt="" />
+          </div>
+          <div className="div2">
+            <img src="./proyectos/1-1/render-cocina-1-1-0002.jpg" alt="" />
+          </div>
+          <div className="div3">
+            <img src="./proyectos/1-1/render-cocina-1-1-0003.jpg" alt="" />
+          </div>
+        </div>
+        <div className="parent">
+        <div className="div1">
+            <img src="./proyectos/1-1/render-cocina-1-1-0001.jpg" alt="" />
+          </div>
+          <div className="div2">
+            <img src="./proyectos/1-1/render-cocina-1-1-0002.jpg" alt="" />
+          </div>
+          <div className="div3">
+            <img src="./proyectos/1-1/render-cocina-1-1-0003.jpg" alt="" />
+          </div>
+        </div>
+        <div className="parent">
+        <div className="div1">
+            <img src="./proyectos/1-1/render-cocina-1-1-0001.jpg" alt="" />
+          </div>
+          <div className="div2">
+            <img src="./proyectos/1-1/render-cocina-1-1-0002.jpg" alt="" />
+          </div>
+          <div className="div3">
+            <img src="./proyectos/1-1/render-cocina-1-1-0003.jpg" alt="" />
+          </div>
+        </div>
       </div>
     </section>
   );

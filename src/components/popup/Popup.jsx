d5c750a2644c6ccbaf092a077b10689e1f47f0c9
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-12-22 13:50:03 +0100

    Mejoras de ajustes en visualización del Popup

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,34 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import './Popup.css'
+import React, { useState, useEffect } from "react";
+import "./Popup.css";
 
 const Popup = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    
+  const [isOpen, setIsOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
     // Temporizador con 3000ms de delay antes de montar el componente
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setIsOpen(true);
-      }, 3000);
-  
-      return () => clearTimeout(timer); // Limpiar el temporizador al desmontar el componente
-    }, []); // Se ejecuta solo una vez al montar el componente
-  
-    const handleClose = () => {
-      setIsOpen(false);
+    const timer = setTimeout(() => {
+      setIsOpen(true);
+    }, 3000);
+
+		// Constante para guardar el ancho de la pantalla donde está cargando el componente Popup
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
     };
-  
-    return (
-      <>
-        {isOpen && (
-          <div className="popup-container">
-            <div className="popup-content">
-              <img src="./popup/felicitacion-navidad-2023.webp" alt="" />
-              <button onClick={handleClose}>Cerrar</button>
-            </div>
-          </div>
-        )}
-      </>
-    );
+
+    window.addEventListener("resize", handleResize);
+
+    return () => clearTimeout(timer); // Limpiar el temporizador al desmontar el componente
+  }, []); // Se ejecuta solo una vez al montar el componente
+
+  const handleClose = () => {
+    setIsOpen(false);
   };
 
-export default Popup
+  return (
+    <>
+      {isOpen && (
+        <div className="popup-container">
+          <div className="popup-content">
+					{windowWidth < 860 ? (
+              <img
+                src="url_de_la_imagen_para_pantallas_pequenas"
+                alt="Feliz Navidad y Próspero Año Nuevo 2024!"
+              />
+            ) : (
+              <img
+                src="./popup/felicitacion-navidad-2023.webp"
+                alt="Felices Fiestas y un próspero año nuevo! Con gratitud, el equipo de @mano!"
+              />
+            )}
+            <button onClick={handleClose}>Cerrar</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Popup;

5d48318cea4a51613fe5739f6284dbe2ed7399d3
Daniel Eduardo Oliveros <daniel@danidev.info>
2023-12-29 11:39:22 +0100

    Agregado Loader en el Blog
    Se agrega un loader animado para mostrar mientras se recupera la data de la API del blog en WP

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFetch } from "../../useFetch";
 import "./Blog.css";
 
 function Blog() {
-  // URL Origen con la info del Blog en JSON.
+  const [loading, setLoading] = useState(true);
   const { data } = useFetch("https://amanoryd.es/wp-json/wp/v2/posts");
 
-  // función para cambiar el formato de la fecha que viene del JSON.
+  // useEffect para manejar el estado de carga
+  useEffect(() => {
+    if (data) {
+      setLoading(false);
+    }
+  }, [data]);
+
   const formatFecha = (fecha) => {
     const options = { year: "numeric", month: "2-digit", day: "2-digit" };
     return new Date(fecha).toLocaleDateString("es-ES", options);
   };
 
-  // Determinar la cantidad de posts a mostrar en función del ancho de la ventana
   const postsToShow = window.innerWidth <= 480 ? 4 : 6;
-
-  // Filtrar las últimas entradas
   const latestPosts = data && data.slice(0, postsToShow);
 
   return (
@@ -29,22 +32,27 @@ function Blog() {
         </p>
       </div>
 
-      <div className="blog-wrap-bottom">
-        <article className="blog-article">
-          {latestPosts &&
-            latestPosts.map((post) => (
-              <span className="blog-item" key={post.id}>
-                <img src={post.uagb_featured_image_src.medium[0]} alt="" />
-                <div className="blog-item-wrap">
-                  <h2>{post.title.rendered}</h2>
-                  <p>{post.yoast_head_json.description}</p>
-                  <h5>{post.yoast_head_json.author}</h5>
-                  <h6>{formatFecha(post.date)}</h6>
-                </div>
-              </span>
-            ))}
-        </article>
-      </div>
+      {loading ? (
+        // Mostrar el loader mientras se cargan los datos
+        <div className="loader"></div>
+      ) : (
+        <div className="blog-wrap-bottom">
+          <article className="blog-article">
+            {latestPosts &&
+              latestPosts.map((post) => (
+                <span className="blog-item" key={post.id}>
+                  <img src={post.uagb_featured_image_src.medium[0]} alt="" />
+                  <div className="blog-item-wrap">
+                    <h2>{post.title.rendered}</h2>
+                    <p>{post.yoast_head_json.description}</p>
+                    <h5>{post.yoast_head_json.author}</h5>
+                    <h6>{formatFecha(post.date)}</h6>
+                  </div>
+                </span>
+              ))}
+          </article>
+        </div>
+      )}
     </section>
   );
 }

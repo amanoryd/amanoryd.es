db27f69b81b2b16adb06d969ef6dbb48928866e2
Daniel Eduardo Oliveros <daniel@danidev.info>
2024-01-10 09:33:43 +0100

    Se comenta línea del componente "Popup"
    Se comenta línea del componente "Popup" ya que no se está llamando temporalmente, esto con la finalidad de evitar errores de renderización en algún navegador.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Componentes del sitio
 import Nav from './components/nav/Nav';
-import Popup from './components/popup/Popup';
+// import Popup from './components/popup/Popup';
 import Hero from './components/hero/Hero';
 import Section1 from './components/section1/Section1';
 import Profesionales from './components/profesionales/Profesionales';

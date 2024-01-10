cf042d1b0d218af37b4b39a733d5f73689646dd7
Daniel Eduardo Oliveros <daniel@danidev.info>
2024-01-10 09:32:40 +0100

    Actualización de Link con componente "Contacto"
    Se actualiza el Link para llamar al componente "Contacto"

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -16,16 +16,16 @@ function Nav() {
     <>
 
     <header>
-      <a href="https://reformas.amanoryd.es" className="Amano, Reformas y Decoración">
+      <Link to="/" className="Amano, Reformas y Decoración">
         <img src="/logov2.svg" alt="Amano, Reformas y Decoración" width={180}/>
-      </a>
+      </Link>
       <div className={`menu-btn ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
         <div className={`navigation ${menuOpen ? "active" : ""}`}>
           <div className="navigation-items">
             <Link to="/">Inicio</ Link>
             <a href="#proyectos">Proyectos</a>
             <Link to="/blog">Blog</Link>
-            <a href="#">Contacto</a>
+            <a href="/contacto">Contacto</a>
             <a href="https://wa.me/34603030316" target="_blank">WhatsApp</a>
               </div>
           </div>
